c37e24ac45f663ee55e74228e7b760414a42ba61
Samuel Christopher <thesamchris@gmail.com>
2017-01-06 21:57:22 -0800

    Added pima diabtes: part two

---
+++ pima-diabetes.py
@@ -0,0 +1,11 @@
+from pandas import read_csv
+
+# Part one: Load the data
+url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data'
+names = ['times_pregnant', 'glucose_conc', 'blood_pressure', 'fold_thickness', 'insulin_serum', 'bmi', 'diabetes_predigree_func', 'age', 'class']
+data = read_csv(url, names=names)
+
+# Part two: Analyze the data
+print(data.head(20))
+print(data.describe())
+print(data.corr())
