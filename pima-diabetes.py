3ec6847eef1946bb7eba42032e7ea535e43fb53c
Samuel Christopher <thesamchris@gmail.com>
2017-01-06 22:18:37 -0800

    Visualized the data

--- pima-diabetes.py
+++ pima-diabetes.py
@@ -1,4 +1,6 @@
 from pandas import read_csv
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
 
 # Part one: Load the data
 url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-diabetes/pima-indians-diabetes.data'
@@ -6,9 +8,18 @@ names = ['times_pregnant', 'glucose_conc', 'blood_pressure', 'fold_thickness', '
 data = read_csv(url, names=names)
 
 # Part two: Analyze the data
-print(data.shape)
-print(data.head(20))
-print(data.describe())
-print(data.corr())
-# Class distrubution
-print(data.groupby('class').size())
+# print(data.shape)
+# print(data.head(20))
+# print(data.describe())
+# print(data.corr())
+# # Class distrubution
+# print(data.groupby('class').size())
+
+# Part three: Visualize the data
+# a. Univariate plots
+data.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
+
+
+# b. Multivariate plots
+scatter_matrix(data)
+plt.show()
