f1edef4e930053b14981bc7724547c1890d0c005
Samuel Christopher <thesamchris@gmail.com>
2017-01-06 19:31:15 -0800

    Day four task: Descriptive statistics

---
+++ four.py
@@ -0,0 +1,8 @@
+from pandas import read_csv
+url = "https://goo.gl/vhm1eU"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(url, names=names)
+
+# Descriptive statistics part
+print(data.describe())
+print(data.corr())
